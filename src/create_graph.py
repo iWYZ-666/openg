e3f71031a802f626410b96d7b122227a735c4e55
Liuyushiii <liuyushi001@163.com>
2023-11-15 10:17:50 +0800

    add batch load

--- src/create_graph.py
+++ src/create_graph.py
@@ -1,10 +1,11 @@
 import re
-from py2neo import Node, Relationship, Graph
+from py2neo import Node, Relationship, Graph, Subgraph
 import os
 import json
 import sqlite3
 import util
 import json
+import time
 
 
 config = json.loads(open(util.get_rel_path('conf'), 'r').read())
@@ -18,11 +19,21 @@ NAME = config['name']
 conn = sqlite3.connect(DB_PATH + DB_NAME)
 my_graph = Graph(URL, password=PASSWORD, name=NAME)
 cur = conn.cursor()
-cur.execute('DROP TABLE graphs')
-conn.commit()
 cur.execute('CREATE TABLE graphs (name TEXT, year NUMBER, month NUMBER)')
 conn.commit()
 
+# load a subgraph into the graph
+def batch_load(node_list: [], link_list: [], graph: Graph):
+    subgraph = Subgraph(node_list, link_list)
+    tx_ = graph.begin()
+    tx_.create(subgraph)
+    graph.commit(tx_)
+    
+# clear the graph
+def clear(graph: Graph):
+    graph.delete_all()
+
+clear(my_graph)
 file_names = os.listdir(JSON_PATH)
 for file_name in file_names:
     pattern = re.compile(r'(202[0123])-(\d+)')
@@ -32,6 +43,8 @@ for file_name in file_names:
     name = file_name[0: end_index - 1]
     insert_sql = f"INSERT INTO graphs values ('{name}', {year}, {month})"
     cur.execute(insert_sql)
+    node_list = []
+    link_list = []
     with open(JSON_PATH + file_name, 'r') as f:
         print('processing ' + file_name)
         js_data = json.loads(f.read())
@@ -41,13 +54,17 @@ for file_name in file_names:
         for node in nodes:
             kind = node['c']
             graph_node = Node(kind, id=node['id'], n=node['n'], v=node['v'], year=year, month=month, repository=name)
-            my_graph.create(graph_node)
+            # my_graph.create(graph_node)
+            node_list.append(graph_node)
             graph_nodes[node['id']] = graph_node
         for link in links:
-            s = graph_nodes[link['s']]
-            t = graph_nodes[link['t']]
-            rel = Relationship(s, t, w=link['w'])
-            my_graph.create(rel)
+            s = graph_nodes.get(link['s'])     
+            t = graph_nodes.get(link['t'])
+            if s and t:
+                rel = Relationship(s, t, w=link['w'])
+                # my_graph.create(rel)
+                link_list.append(rel)
+        batch_load(node_list, link_list, my_graph)
     conn.commit()
 cur.close()
 conn.close()
