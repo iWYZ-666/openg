8978110e6cf460d4f6ee26c75a5d93375cc3e8f5
Liuyushiii <liuyushi001@163.com>
2023-11-20 22:51:11 +0800

    chore(global): add prepare and run script

--- prepare.sh
+++ prepare.sh
@@ -1,6 +1,6 @@
 pip install -r requirements.txt
 python3 src/download_json.py
-docker run \
+docker run -d \
     --publish=7474:7474 --publish=7687:7687 \
     --volume=$HOME/neo4j/data:/data \
     neo4j
